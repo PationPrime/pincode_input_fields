2d0ba813591846c95976c242764146110637a3f2
PationPrime <pationprime@gmail.com>
2024-04-30 16:14:45 +0300

    beatiful example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,9 +11,29 @@ class PincodeInputFieldsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
         home: Scaffold(
+          backgroundColor: const Color(0xFF1A1B20),
           body: Center(
             child: PincodeInputFields(
-              controller: PincodeInputFieldsController(),
+              length: 6,
+              heigth: 54,
+              width: 51,
+              borderRadius: BorderRadius.circular(9),
+              unfocusBorder: Border.all(
+                width: 1,
+                color: const Color(0xFF5B6774),
+              ),
+              focusBorder: Border.all(
+                width: 1,
+                color: const Color(0xFF9B71F4),
+              ),
+              cursorColor: Colors.white,
+              cursorWidth: 2,
+              focusFieldColor: const Color(0xFF2A2B32),
+              unfocusFieldColor: const Color(0xFF2A2B32),
+              textStyle: const TextStyle(
+                color: Colors.white,
+                fontSize: 21,
+              ),
             ),
           ),
         ),
